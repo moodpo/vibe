3ba4e177d3a37e5f3d3a2efc18bdeec3d2a6e463
yangchw <yangchw@teamsun.com.cn>
2025-07-24 16:19:11 +0800

    03: trpc-setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,23 @@
+import { Suspense } from "react";
+
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Client } from "./client";
+
 const Page = async () => {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(
+    trpc.createAI.queryOptions({
+      text: "YXiaoxie PREFETCH",
+    })
+  );
+
   return (
-    <div>
-      Hello World
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Client />
+      </Suspense>
+    </HydrationBoundary>
   );
 };
 
